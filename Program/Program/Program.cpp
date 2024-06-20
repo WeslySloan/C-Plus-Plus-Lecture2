42fafe458d2777c57a5165ff4d9380866ff19db8
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-06-20 19:46:46 +0900

    (2024.06.20) 해시 테이블

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,135 +1,98 @@
-﻿#include <iostream>
-#include <vector>
+﻿
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+
 
 using namespace std;
 
-template <typename T>
-class Vector
+class String
 {
 private:
     int size;
-    int capacity;
-    T* container;
+    char * container;
+
 
 public:
-    Vector()
+    String()
     {
         size = 0;
-
-        capacity = 0;
         container = nullptr;
-
-        // capacity = 1; 
-        // container = new T[capacity];
     }
-
-    void PushBack(T data)
+    
+    void operator = (const char* content)
     {
-        if (capacity <= 0)
-        {
-            Resize(1);
-        }
-        else if (size >= capacity)
-        {
-            Resize(capacity * 2);
-        }
+        int arraySize = strlen(content) + 1;
 
-        container[size++] = data;
+
+
+        if (container == nullptr)
+        {
+            container = new char[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                container[i] = content[i];
+            }
+        }
+        else
+        {
+            char* newContainer = new char[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                newContainer[i] = content[i];
+            }
+
+            delete container;
+
+            container = newContainer;
+        }
+        size = strlen(container);
     }
 
-    void Resize(int newSize)
+    int& Size()
     {
-        // 1. capacity에 새로운 size값을 저장합니다.
-        capacity = newSize;
-
-        // 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
-        //    메모리 공간을 가리키도록 합니다.
-        T* newContainer = new T[capacity];
-
-        // 3. 새로운 메모리 공간의 값을 초기화합니다.
-        for (int i = 0; i < capacity; i++)
-        {
-            newContainer[i] = T(); // 기본 생성자 호출
-        }
-
-        // 4. 기존 배열에 있는 값을 복사해서 새로운 배열에
-        //    넣어줍니다.
-        for (int i = 0; i < size; i++)
-        {
-            newContainer[i] = container[i];
-        }
-
-        // 5. 기존 배열의 메모리를 해제합니다.
-        if (container != nullptr)
-        {
-            delete[] container;
-        }
-
-        // 6. 기존에 배열을 가리키던 포인터 변수의 값을
-        //    새로운 배열의 시작 주소로 가리킵니다.
-        container = newContainer;
+        // int len = strlen(container);
+        return size;
     }
 
-    T & operator [](const int & index)
+    char& operator [] (int index)
     {
         return container[index];
     }
 
-    int & Size()
+    int Compare(const char * content)
     {
-        return size;
-    }
-
-    ~Vector()
-    {
-        if (container != nullptr)
+        if (content == container)
         {
-            delete[] container;
+            return 0;
+        }
+        else
+        {
+
         }
     }
 
-    // void PushBack(T data)
-    // {
-    //     if (size >= capacity)
-    //     {
-    //         Resize(capacity * 2);
-    //     }
-    // 
-    //     container[size++] = data;
-    // }
-    // 
-    // T& operator[](int index)
-    // {
-    //     if (index < 0 || index >= size)
-    //     {
-    //         cout << "Index" << endl;
-    //     }
-    //     return container[index];
-    // }
-    // 
-    // int Size()
-    // {
-    //     return size;
-    // }
-    // 
-    // int Capacity()
-    // {
-    //     return capacity;
-    // }
 };
 
 int main()
 {
-    Vector<int> myVector;
+    // String string;
+    // 
+    // string = "Janna";
+    // 
+    // cout << "string의 크기 : " << string.Size() << endl;
+    // 
+    // string = "Alistar";
+    // 
+    // for (int i = 0; i < string.Size(); i++)
+    // {
+    //     cout << string[i] 
+    // }
 
-    myVector.PushBack(10);
-    myVector.PushBack(20);
-    myVector.PushBack(30);
+    string name;
 
-    for (int i = 0; i < myVector.Size(); i++)
-    {
-        cout << myVector[i] << endl;
-    }
+    name.compare("qd");
 
     return 0;
 }
