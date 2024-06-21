2bef568b28c311dd46f64c49855d82f26de5a5be
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-06-21 23:41:10 +0900

    (2024.06.21) 힙

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,98 +1,44 @@
-﻿
-#include <iostream>
-#define _CRT_SECURE_NO_WARNINGS
-
+﻿﻿#include <iostream>
 
 using namespace std;
 
-class String
+#define SIZE 6
+
+template<typename KEY, typename VALUE>
+class HashTable
 {
 private:
-    int size;
-    char * container;
+	struct Node
+	{
+		KEY key;
+		VALUE value;
 
+		Node* next;
+	};
+
+	struct Bucket
+	{
+		int count;
+		Node* head;
+	};
+
+	Bucket bucket[SIZE];
 
 public:
-    String()
-    {
-        size = 0;
-        container = nullptr;
-    }
-    
-    void operator = (const char* content)
-    {
-        int arraySize = strlen(content) + 1;
+	HashTable()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
+		}
 
-
-
-        if (container == nullptr)
-        {
-            container = new char[arraySize];
-
-            for (int i = 0; i < arraySize; i++)
-            {
-                container[i] = content[i];
-            }
-        }
-        else
-        {
-            char* newContainer = new char[arraySize];
-
-            for (int i = 0; i < arraySize; i++)
-            {
-                newContainer[i] = content[i];
-            }
-
-            delete container;
-
-            container = newContainer;
-        }
-        size = strlen(container);
-    }
-
-    int& Size()
-    {
-        // int len = strlen(container);
-        return size;
-    }
-
-    char& operator [] (int index)
-    {
-        return container[index];
-    }
-
-    int Compare(const char * content)
-    {
-        if (content == container)
-        {
-            return 0;
-        }
-        else
-        {
-
-        }
-    }
+	}
 
 };
 
 int main()
 {
-    // String string;
-    // 
-    // string = "Janna";
-    // 
-    // cout << "string의 크기 : " << string.Size() << endl;
-    // 
-    // string = "Alistar";
-    // 
-    // for (int i = 0; i < string.Size(); i++)
-    // {
-    //     cout << string[i] 
-    // }
 
-    string name;
-
-    name.compare("qd");
-
-    return 0;
+	return 0;
 }
