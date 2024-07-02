20f24068a7c8d053ef6f4ef3707bc33ce7fd08db
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-07-02 19:46:28 +0900

    (2024.07.02) 레드 블랙 트리

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -15,15 +15,192 @@ private:
 	};
 
 	Node* root;
+
 public:
-	BinarySearchTree()
+    BinarySearchTree()
+    {
+        root = nullptr;
+    }
+
+    Node* Root()
+    {
+        return root;
+    }
+
+	Node* CreateNode(T data)
 	{
-		root = nullptr;
+		Node* newNode = new Node();
+		newNode->data = data;
+		newNode->left = nullptr;
+		newNode->right = nullptr;
+
+		return newNode;
 	}
 
+	void Insert(T data)
+	{
+		if (root == nullptr)
+		{
+			root = CreateNode(data);
+		}
+		else
+		{
+			Node* currentNode = root;
+
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data == data)
+				{
+					return;
+				}
+				else if (currentNode->data > data)
+				{
+					if (currentNode->left == nullptr)
+					{
+						currentNode->left = CreateNode(data);
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->left;
+					}
+				}
+				else
+				{
+					if (currentNode->right == nullptr)
+					{
+						currentNode->right = CreateNode(data);
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+
+			}
+		}
+	}
+
+	bool Find(T data)
+	{
+		Node * currentNode = root;
+
+		if (currentNode == nullptr)
+		{
+			return false;
+		}
+		else
+		{
+			if (currentNode->data == data)
+			{
+				return true;
+			}
+			else
+			{
+				if (currentNode->data > data)
+				{
+					currentNode = currentNode->left;
+				}
+				else
+				{
+					currentNode = currentNode->right;
+				}
+			}
+		}
+	}
+
+	void Destroy(Node* root)
+	{
+		if (root != nullptr)
+		{
+			Destroy(root->left);
+			Destroy(root->right);
+			delete root;
+		}
+	}
+
+	~BinarySearchTree()
+	{
+		Destroy(root);
+	}
+
+	void Inorder(Node* root)
+	{
+		if (root != nullptr)
+		{
+			Inorder(root->left);
+			cout << root->data << " ";
+			Inorder(root->right);
+		}
+	}
+
+    /*void Insert(T data)
+    {
+        if (root == nullptr)
+        {
+            root = CreateNode(data);
+            return;
+        }
+
+        Node* current = root;
+        Node* parent = nullptr;
+
+        while (current != nullptr)
+        {
+            parent = current;
+            if (data < current->data)
+            {
+                current = current->left;
+            }
+            else
+            {
+                current = current->right;
+            }
+        }
+
+        if (data < parent->data)
+        {
+            parent->left = CreateNode(data);
+        }
+        else
+        {
+            parent->right = CreateNode(data);
+        }
+    }
+
+    void Inorder(Node* root)
+    {
+        if (root != nullptr)
+        {
+            Inorder(root->left);
+            cout << root->data << " ";
+            Inorder(root->right);
+        }
+    }
+
+    void Inorder()
+    {
+        Inorder(root);
+    }*/
 };
 int main()
 {
+    BinarySearchTree<int> binarysearchtree;
+
+    binarysearchtree.Insert(40);
+    binarysearchtree.Insert(80);
+    binarysearchtree.Insert(70);
+    binarysearchtree.Insert(60);
+    binarysearchtree.Insert(50);
+
+    binarysearchtree.Insert(20);
+    binarysearchtree.Insert(30);
+
+	cout << binarysearchtree.Find(20) << endl;
+
+    binarysearchtree.Inorder(binarysearchtree.Root());
+
 
 	return 0;
 }
